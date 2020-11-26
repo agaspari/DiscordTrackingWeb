1cb5f26f8050328e16e7301e4e8358d01dbd208d
Antonio <26602603+agaspari@users.noreply.github.com>
2020-11-26 12:14:11 -0800

    Added created since

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -5,22 +5,37 @@ export default class User extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            userData: {}
         }
     }
 
     componentDidMount() {
-       
+        const { userId } = this.props.match.params;
+
+        fetch(`${window.location.protocol}//${window.location.hostname}:4000/api/users/${userId}?guildId=${'518686827096440832'}`, {
+            method: "GET",
+        })
+        .then(res => res.json())
+        .then(userData => {
+            this.setState({ userData });
+        });
     }
 
     render() {
         const { userId } = this.props.match.params;
+        const { userData } = this.state;
 
         return (
             <div>
-                <MessageTrendUser
-                    userId={userId}
-                />
+                <div>
+                    <h1>{userData.nickname}</h1>
+                    <p>Account Created: {userData.age}</p>
+                </div>
+                <div>
+                    <MessageTrendUser
+                        userId={userId}
+                    />
+                </div>
             </div>
         ); 
     }
